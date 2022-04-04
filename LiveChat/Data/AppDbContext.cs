621419428dd25bba660f8e6f438bd123b0111473
LeeMustache <czerwonapanda1@interia.pl>
2022-04-04 22:21:11 +0200

    Add db context

---
+++ LiveChat/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using LiveChat.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveChat.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Message> Messages => Set<Message>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
