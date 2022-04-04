94cc6bce42d54947367cfe57f9ecbeaef158f8f0
LeeMustache <czerwonapanda1@interia.pl>
2022-04-04 22:13:48 +0200

    Add models

---
+++ LiveChat/Dto/MessageRecieve.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using LiveChat.Domain;
+
+namespace LiveChat.Dto;
+
+public record class MessageRecieveDto(
+    [MaxLength(ValidationConsts.MaxMessageLength)] string Message,
+    string AuthorId,
+    DateTime Date,
+    string ProfilePicture
+);
