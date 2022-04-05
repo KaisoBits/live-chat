0bfb5ac7cf063310019af3749b21bf7df3bce624
LeeMustache <czerwonapanda1@interia.pl>
2022-04-05 14:39:29 +0200

    Add Identity

--- LiveChat/Data/AppDbContext.cs
+++ LiveChat/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using LiveChat.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LiveChat.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<User>
 {
     public DbSet<Message> Messages => Set<Message>();
 
